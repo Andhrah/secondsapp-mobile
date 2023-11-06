7d9a59153416ab965b8c4053beb4a8490e706028
Alexandra Collins <andracolexa@gmail.com>
2023-11-06 14:23:48 +0100

    implement image data

---
+++ src/constant/imageData.tsx
@@ -0,0 +1,25 @@
+export type ImageItem = {
+  id: string;
+  uri: any;
+  selected?: boolean;
+};
+
+const images: ImageItem[] = [
+  { id: '1', uri: require("../../assets/images/img1_unsplash.jpg")},
+  { id: '2', uri: require("../../assets/images/img2-unsplash.jpg")},
+  { id: '3', uri: require("../../assets/images/img3-unsplash.jpg")},
+  { id: '4', uri: require("../../assets/images/img4-unsplash.jpg")},
+  { id: '5', uri: require("../../assets/images/img5-unsplash.jpg")},
+  { id: '6', uri: require("../../assets/images/img6-unsplash.jpg")},
+  { id: '7', uri: require("../../assets/images/img7-unsplash.jpg")},
+  { id: '8', uri: require("../../assets/images/img8-unsplash.jpg")},
+  { id: '9', uri: require("../../assets/images/img9-unsplash.jpg")},
+  { id: '10', uri: require("../../assets/images/img10-unsplash.jpg")},
+  { id: '11', uri: require("../../assets/images/img11-unsplash.jpg")},
+  { id: '12', uri: require("../../assets/images/img12-unsplash.jpg")},
+  { id: '13', uri: require("../../assets/images/img13-unsplash.jpg")},
+  { id: '14', uri: require("../../assets/images/img14-unsplash.jpg")},
+  { id: '15', uri: require("../../assets/images/img15-unsplash.jpg")},
+];
+
+export default images;
